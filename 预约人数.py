b0cc374ae8a9bd1eb1358673ab76cd253e2ec069
jin81134 <811343804@qq.com>
2023-04-03 22:44:15 +0800

    代码修正

--- 预约人数.py
+++ 预约人数.py
@@ -10,9 +10,9 @@ file_name = "预约数据.csv"  # 数据保存位置
 file_error_name = "预约数据-出错信息.txt"  # 错误信息位置
 
 while True:
-    try:
 
-        # "蔚蓝档案官网数据"
+    # "蔚蓝档案官网数据"
+    try:
         url = 'https://bluearchive-cn.com/api/pre-reg/stats'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
@@ -22,7 +22,19 @@ while True:
         Ba_ba_api = data_dict['data']['count']
         print("官网数据 " + str(Ba_ba_api))
 
-        # "Bilibili数据"
+    except Exception as e:
+        # 获取当前时间
+        now = datetime.now()
+        minute = now.strftime("%Y-%m-%d %H:%M")  # 格式化为精确到分钟的字符串
+        print("当前时间" + minute)
+        # 输出出错信息到文件
+        with open(file_error_name, mode='a', newline='') as f:
+            f.write(minute + " 错误信息 蔚蓝档案官网 " + str(e) + '\n')
+        print("Error:", e)
+        Ba_ba_api = -1
+
+    # "Bilibili数据"
+    try:
         url = 'https://line1-h5-pc-api.biligame.com/game/detail/gameinfo?game_base_id=109864&ts=1680239994877&request_id=cbniUTkIyXpfwxAB83WGglr8getUcGIP&appkey=h9Ejat5tFh81cq8V&sign=b7c535ab5c6b5c9ca718dd430e8928bc'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
@@ -32,7 +44,19 @@ while True:
         Bili_ba_api = data_dict['data']['book_num']
         print("B站数据 " + str(Bili_ba_api))
 
-        # "TapTap数据"
+    except Exception as e:
+        # 获取当前时间
+        now = datetime.now()
+        minute = now.strftime("%Y-%m-%d %H:%M")  # 格式化为精确到分钟的字符串
+        print("当前时间" + minute)
+        # 输出出错信息到文件
+        with open(file_error_name, mode='a', newline='') as f:
+            f.write(minute + " 错误信息 B站 " + str(e) + '\n')
+        print("Error:", e)
+        Bili_ba_api = -1
+
+    # "TapTap数据"
+    try:
         url = 'https://www.taptap.cn/webapiv2/app/v2/detail-by-id/316964?X-UA=V%3D1%26PN%3DWebApp%26LANG%3Dzh_CN%26VN_CODE%3D100%26VN%3D0.1.0%26LOC%3DCN%26PLT%3DPC%26DS%3DAndroid%26UID%3D403e642a-90ca-42a4-9bef-faa806d58dc1%26VID%3D3126483%26DT%3DPC%26OS%3DWindows%26OSV%3D10'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
@@ -41,7 +65,19 @@ while True:
         data_dict = json.loads(response.text)
         Tap_ba_api = data_dict['data']['stat']['reserve_count']
         print("Tap数据 " + str(Tap_ba_api))
+    except Exception as e:
+        # 获取当前时间
+        now = datetime.now()
+        minute = now.strftime("%Y-%m-%d %H:%M")  # 格式化为精确到分钟的字符串
+        print("当前时间" + minute)
+        # 输出出错信息到文件
+        with open(file_error_name, mode='a', newline='') as f:
+            f.write(minute + " 错误信息 Tap " + str(e) + '\n')
+        print("Error:", e)
+        Tap_ba_api = -1
 
+    #   写入文件
+    try:
         sum = Ba_ba_api + Bili_ba_api + Tap_ba_api
         print("合计人数 " + str(sum))
 
@@ -73,15 +109,13 @@ while True:
                         writer.writerow(file_write)  # 写入变量数据
                         exit(1)
         else:
-            print("CSV文件不存在")
+            print("CSV文件不存在，已创建文件 " + file_name)
             with open(file_name, mode='a', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(field_names)  # 写入CSV文件的表头
                 writer.writerow(file_write)  # 写入变量数据
-        time.sleep(60)
 
     except Exception as e:
-
         # 获取当前时间
         now = datetime.now()
         minute = now.strftime("%Y-%m-%d %H:%M")  # 格式化为精确到分钟的字符串
@@ -89,6 +123,6 @@ while True:
 
         # 输出出错信息到文件
         with open(file_error_name, mode='a', newline='') as f:
-            f.write(minute + " 错误信息 " + str(e) + '\n')
+            f.write(minute + " 错误信息 写入文件 " + str(e) + '\n')
         print("Error:", e)
-        time.sleep(60)
+    time.sleep(60)
