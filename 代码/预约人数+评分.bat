221fe6121ea22f0dd3e8375b90aa2d53de1cbc63
jin <811343804@qq.com>
2023-04-09 23:08:06 +0800

    编码改变

--- 代码/预约人数+评分.bat
+++ 代码/预约人数+评分.bat
@@ -1,93 +1,93 @@
 @echo off
-rem éœ€è¦ä½¿ç”¨è½¯ä»¶jqï¼Œç¡®ä¿å…¶ç¯å¢ƒå˜é‡æ­£ç¡®
-rem set /a ä¸èƒ½ä¸º0
+rem ĞèÒªÊ¹ÓÃÈí¼şjq£¬È·±£Æä»·¾³±äÁ¿ÕıÈ·
+rem set /a ²»ÄÜÎª0
 
 cd  %tmp%
-set "path1=D:\æ•°æ®\é¢„çº¦æ•°æ®\"
-set "path2=D:\æ•°æ®\è¯„åˆ†æŠ“å–\"
-set "file1=%path1%é¢„çº¦æ•°æ®.csv"
-set "file2=%path2%è”šè“æ¡£æ¡ˆè¯„åˆ†æŠ“å–_Bç«™.csv"
-set "file3=%path2%è”šè“æ¡£æ¡ˆè¯„åˆ†æŠ“å–_Tap.csv"
+set "path1=D:\Êı¾İ\Ô¤Ô¼Êı¾İ\"
+set "path2=D:\Êı¾İ\ÆÀ·Ö×¥È¡\"
+set "file1=%path1%Ô¤Ô¼Êı¾İ.csv"
+set "file2=%path2%ÎµÀ¶µµ°¸ÆÀ·Ö×¥È¡_BÕ¾.csv"
+set "file3=%path2%ÎµÀ¶µµ°¸ÆÀ·Ö×¥È¡_Tap.csv"
 
 if not exist "%path1%" (
     mkdir "%path1%"
-    echo å·²åˆ›å»ºæ–‡ä»¶å¤¹: %path1%
+    echo ÒÑ´´½¨ÎÄ¼ş¼Ğ: %path1%
     ) else (
-    echo æ–‡ä»¶å¤¹å·²å­˜åœ¨: %path1%
+    echo ÎÄ¼ş¼ĞÒÑ´æÔÚ: %path1%
 )
 
 if not exist "%path2%" (
     mkdir "%path2%"
-    echo å·²åˆ›å»ºæ–‡ä»¶å¤¹: %path2%
+    echo ÒÑ´´½¨ÎÄ¼ş¼Ğ: %path2%
 ) else (
-    echo æ–‡ä»¶å¤¹å·²å­˜åœ¨: %path2%
+    echo ÎÄ¼ş¼ĞÒÑ´æÔÚ: %path2%
 )
 
 IF EXIST %file1% (
-    REM æ–‡ä»¶å­˜åœ¨æ—¶è¦æ‰§è¡Œçš„å‘½ä»¤
+    REM ÎÄ¼ş´æÔÚÊ±ÒªÖ´ĞĞµÄÃüÁî
 ) ELSE (
-    echo æ—¶é—´,å®˜ç½‘æ•°æ®,Bç«™æ•°æ®,Tapæ•°æ®,åˆè®¡äººæ•°>>"%file1%"
+    echo Ê±¼ä,¹ÙÍøÊı¾İ,BÕ¾Êı¾İ,TapÊı¾İ,ºÏ¼ÆÈËÊı>>"%file1%"
 )
 
 IF EXIST %file2%  (
-    REM æ–‡ä»¶å­˜åœ¨æ—¶è¦æ‰§è¡Œçš„å‘½ä»¤
+    REM ÎÄ¼ş´æÔÚÊ±ÒªÖ´ĞĞµÄÃüÁî
 ) ELSE (
-    echo æ—¶é—´,Bilibiliè¯„åˆ†,è¯„ä»·äººæ•°,1æ˜Ÿ,2æ˜Ÿ,3æ˜Ÿ,4æ˜Ÿ,5æ˜Ÿ,è¯„ä»·äººæ•°-è¯„åˆ†äººæ•°>>"%file2%"
+    echo Ê±¼ä,BilibiliÆÀ·Ö,ÆÀ¼ÛÈËÊı,1ĞÇ,2ĞÇ,3ĞÇ,4ĞÇ,5ĞÇ,ÆÀ¼ÛÈËÊı-ÆÀ·ÖÈËÊı>>"%file2%"
 )
 
 IF EXIST  %file3%  (
-    REM æ–‡ä»¶å­˜åœ¨æ—¶è¦æ‰§è¡Œçš„å‘½ä»¤
+    REM ÎÄ¼ş´æÔÚÊ±ÒªÖ´ĞĞµÄÃüÁî
 ) ELSE (
-    echo æ—¶é—´,Tapè¯„åˆ†,è¯„ä»·äººæ•°,1æ˜Ÿ,2æ˜Ÿ,3æ˜Ÿ,4æ˜Ÿ,5æ˜Ÿ,è¯„ä»·äººæ•°-è¯„åˆ†äººæ•°>>"%file3%"
+    echo Ê±¼ä,TapÆÀ·Ö,ÆÀ¼ÛÈËÊı,1ĞÇ,2ĞÇ,3ĞÇ,4ĞÇ,5ĞÇ,ÆÀ¼ÛÈËÊı-ÆÀ·ÖÈËÊı>>"%file3%"
 )
 
-set "expected_header=æ—¶é—´,å®˜ç½‘æ•°æ®,Bç«™æ•°æ®,Tapæ•°æ®,åˆè®¡äººæ•°"
+set "expected_header=Ê±¼ä,¹ÙÍøÊı¾İ,BÕ¾Êı¾İ,TapÊı¾İ,ºÏ¼ÆÈËÊı"
 set /p header=<%file1%
 if "%header%"=="%expected_header%" (
-  echo è¡¨å¤´æ­£ç¡®.
+  echo ±íÍ·ÕıÈ·.
 ) else (
-  echo è¡¨å¤´é”™è¯¯æˆ–æ— ï¼Œè¯·æ£€æŸ¥ï¼[%expected_header%]
-  echo æ–‡ä»¶åç§° %file1%
+  echo ±íÍ·´íÎó»òÎŞ£¬Çë¼ì²é£¡[%expected_header%]
+  echo ÎÄ¼şÃû³Æ %file1%
   exit /b 1
 )
 
-set "expected_header=æ—¶é—´,Bilibiliè¯„åˆ†,è¯„ä»·äººæ•°,1æ˜Ÿ,2æ˜Ÿ,3æ˜Ÿ,4æ˜Ÿ,5æ˜Ÿ,è¯„ä»·äººæ•°-è¯„åˆ†äººæ•°"
+set "expected_header=Ê±¼ä,BilibiliÆÀ·Ö,ÆÀ¼ÛÈËÊı,1ĞÇ,2ĞÇ,3ĞÇ,4ĞÇ,5ĞÇ,ÆÀ¼ÛÈËÊı-ÆÀ·ÖÈËÊı"
 set /p header=<%file2%
 if "%header%"=="%expected_header%" (
-  echo è¡¨å¤´æ­£ç¡®.
+  echo ±íÍ·ÕıÈ·.
 ) else (
-  echo è¡¨å¤´é”™è¯¯æˆ–æ— ï¼Œè¯·æ£€æŸ¥ï¼[%expected_header%]
-  echo æ–‡ä»¶åç§° %file2%
+  echo ±íÍ·´íÎó»òÎŞ£¬Çë¼ì²é£¡[%expected_header%]
+  echo ÎÄ¼şÃû³Æ %file2%
   exit /b 1
 )
 
-set "expected_header=æ—¶é—´,Tapè¯„åˆ†,è¯„ä»·äººæ•°,1æ˜Ÿ,2æ˜Ÿ,3æ˜Ÿ,4æ˜Ÿ,5æ˜Ÿ,è¯„ä»·äººæ•°-è¯„åˆ†äººæ•°"
+set "expected_header=Ê±¼ä,TapÆÀ·Ö,ÆÀ¼ÛÈËÊı,1ĞÇ,2ĞÇ,3ĞÇ,4ĞÇ,5ĞÇ,ÆÀ¼ÛÈËÊı-ÆÀ·ÖÈËÊı"
 set /p header=<%file3%
 if "%header%"=="%expected_header%" (
-  echo è¡¨å¤´æ­£ç¡®.
+  echo ±íÍ·ÕıÈ·.
 ) else (
-  echo è¡¨å¤´é”™è¯¯æˆ–æ— ï¼Œè¯·æ£€æŸ¥ï¼[%expected_header%]
-  echo æ–‡ä»¶åç§° %file3%
+  echo ±íÍ·´íÎó»òÎŞ£¬Çë¼ì²é£¡[%expected_header%]
+  echo ÎÄ¼şÃû³Æ %file3%
   exit /b 1
 )
 
 :loop
 
-rem "è”šè“æ¡£æ¡ˆå®˜ç½‘æ•°æ®-é¢„çº¦äººæ•°"
+rem "ÎµÀ¶µµ°¸¹ÙÍøÊı¾İ-Ô¤Ô¼ÈËÊı"
 set "url=https://bluearchive-cn.com/api/pre-reg/stats"
 set "user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 curl -s -H "User-Agent: %user_agent%" "%url%" > %tmp%\response.txt
 for /f "delims=" %%i in ('type response.txt ^| jq -r ".data.count"') do set "Ba_ba_api=%%i"
 echo %Ba_ba_api%>> "%file1%"
 
-rem "Bilibiliæ•°æ®-é¢„çº¦äººæ•°"
+rem "BilibiliÊı¾İ-Ô¤Ô¼ÈËÊı"
 set "url=https://line1-h5-pc-api.biligame.com/game/detail/gameinfo?game_base_id=109864&ts=1680239994877&request_id=cbniUTkIyXpfwxAB83WGglr8getUcGIP&appkey=h9Ejat5tFh81cq8V&sign=b7c535ab5c6b5c9ca718dd430e8928bc"
 set "user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 curl -s -H "User-Agent: %user_agent%" "%url%" > %tmp%response.txt
 for /f "delims=" %%i in ('type response.txt ^| jq -r ".data.book_num"') do set "Bili_ba_api=%%i"
 echo %Bili_ba_api%>> "%file1%"
 
-rem "Bilibiliè¯„ä»·æ•°æ®"
+rem "BilibiliÆÀ¼ÛÊı¾İ"
 set "url=https://line1-h5-pc-api.biligame.com/game/comment/summary?game_base_id=109864&ts=1680585215754&request_id=ZgNwydOaxhj62MXUz3L1FgYrnIsudEgC&appkey=h9Ejat5tFh81cq8V&sign=39b48ab6cd61acf044cda2493af9d15e"
 set "user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 curl -s -H "User-Agent: %user_agent%" "%url%" > %tmp%\response.txt
@@ -100,8 +100,8 @@ for /f "delims=" %%i in ('type response.txt ^| jq -r ".data.star_number_list[2]"
 for /f "delims=" %%i in ('type response.txt ^| jq -r ".data.star_number_list[3]"') do set "stra_4=%%i"
 for /f "delims=" %%i in ('type response.txt ^| jq -r ".data.star_number_list[4]"') do set "stra_5=%%i"
 
-rem TapTapæ•°æ®-é¢„çº¦äººæ•°ä¸è¯„åˆ†
-rem åœ¨batä¸­å¦‚æœç½‘ç½‘å€ä¸­æœ‰%åˆ™éœ€è¦å†åŠ ä¸ªç™¾åˆ†å·è½¬ä¹‰
+rem TapTapÊı¾İ-Ô¤Ô¼ÈËÊıÓëÆÀ·Ö
+rem ÔÚbatÖĞÈç¹ûÍøÍøÖ·ÖĞÓĞ%ÔòĞèÒªÔÙ¼Ó¸ö°Ù·ÖºÅ×ªÒå
 set "user_agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
 set "url=https://www.taptap.cn/webapiv2/app/v2/detail-by-id/316964?X-UA=V%%3D1%%26PN%%3DWebApp%%26LANG%%3Dzh_CN%%26VN_CODE%%3D100%%26VN%%3D0.1.0%%26LOC%%3DCN%%26PLT%%3DPC%%26DS%%3DAndroid%%26UID%%3D403e642a-90ca-42a4-9bef-faa806d58dc1%%26VID%%3D3126483%%26DT%%3DPC%%26OS%%3DWindows%%26OSV%%3D10" > response.txt
 curl -s -H "User-Agent: %user_agent%" "%url%" > %tmp%\response.txt
@@ -123,7 +123,7 @@ set "minute=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%"
 echo "%minute%","%minute%","%Bili_ba_api%","%Tap_ba_api%","%sum%">>"%file1%"
 echo "%minute%","%Bili_ba_api0%","%Bili_ba_api1%","%stra_1%","%stra_2%","%stra_3%","%stra_4%","%stra_5%","%Difference_value1%">>"%file2%"
 echo "%minute%","%Tap_ba_api0%","%Tap_ba_api1%","%Tap_ba_api2%","%Tap_ba_api3%","%Tap_ba_api4%","%Tap_ba_api5%","%Tap_ba_api6%","%Difference_value2%">>"%file3%"
-echo å½“å‰æ—¶é—´---%minute%---
+echo µ±Ç°Ê±¼ä---%minute%---
 for /f "tokens=1 delims==" %%a in ('set Bili_ba_api') do set %%a=-1
 for /f "tokens=1 delims==" %%a in ('set Tap_ba_api') do set %%a=-1
 for /f "tokens=1 delims==" %%a in ('set stra_') do set %%a=-1
